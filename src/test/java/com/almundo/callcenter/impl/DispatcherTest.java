21c4e8e8f5e1e87ade9c4179bbddf3a0fa2caa26
jhon00021 <jhon00021@gmail.com>
2018-08-06 08:58:15 -0500

    adding some test

--- src/test/java/com/almundo/callcenter/impl/DispatcherTest.java
+++ src/test/java/com/almundo/callcenter/impl/DispatcherTest.java
@@ -1,6 +1,47 @@
 package com.almundo.callcenter.impl;
 
+import com.almundo.callcenter.model.Employee;
+import com.almundo.callcenter.service.Dispatcher;
+import com.almundo.callcenter.service.GenerateQueue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DispatcherTest {
+
+    @Autowired
+    private GenerateQueue generateQueue;
+
+    @Autowired
+    private Dispatcher dispatcher;
+
+    private PriorityBlockingQueue<Employee> queue;
+
+    @Before
+    public void init(){
+        queue = generateQueue.generateQueueEmployees();
+    }
+
+    @Test
+    public void validateConf(){
+        int sizeBeforeRun = queue.size();
+        dispatcher.setPriorityBlockingQueue(queue);
+        dispatcher.dispatchCall();
+
+        //assertThat(queue.size()).isLessThan(sizeBeforeRun);
+
+    }
+
+
 
 
 }
